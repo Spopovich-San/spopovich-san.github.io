f1a2bfb513b8811b790c55c57e235149aa09f025
Spopovich-San <xhmn107.7@gmail.com>
2025-08-22 22:52:46 -0600

    Add files via upload

--- bellosrecuerdos-app/js/lunaradio-sincors.js
+++ bellosrecuerdos-app/js/lunaradio-sincors.js
@@ -1,95 +1,51 @@
-(function($){
-    $.fn.lunaradio = function(options) {
+/*
+  LunaRadio Modificado: Solo interfaz visual (metadatos, logo y nombre)
+*/
 
+(function($) {
+    $.fn.lunaradio = function(options) {
         var settings = $.extend({
-            userinterface: "big",
-            backgroundcolor: "#000000",
-            fontcolor: "#ffffff",
-            hightlightcolor: "#FF6767",
-            fontname: "Bebas Neue",
-            googlefont: "Bebas+Neue&display=swap",
-            fontratio: "0.4",
-            radioname: "My Radio",
-            scroll: "true",
-            coverimage: "",
-            onlycoverimage: "false",
-            coverstyle: "square",
+            radioname: "Radio",
             streamurl: "",
             streamtype: "shoutcast2",
             shoutcastpath: "/stream",
             shoutcastid: "1",
-            itunestoken: "1000lIPN",
-            metadatainterval: "5000",
-            autoplay: "false"
+            itunestoken: "",
+            coverimage: "",
+            coverstyle: "square",
+            fontcolor: "#ffffff",
+            backgroundcolor: "#000000",
+            hightlightcolor: "#FF6767",
+            googlefont: "Bebas+Neue",
+            fontname: "Bebas Neue",
+            fontratio: "0.4",
+            metadatainterval: 5000,
         }, options);
 
-        return this.each(function(){
-            var container = $(this);
-            container.empty();
-            container.css({
-                "background-color": settings.backgroundcolor,
-                "color": settings.fontcolor,
-                "font-family": settings.fontname,
-                "text-align": "center",
-                "padding": "20px"
+        var container = this;
+        container.html(`
+          <div style="text-align:center; padding-top:40px;">
+            <div id="luna-cover">
+              <img src="${settings.coverimage}" style="width:150px; height:150px; object-fit:cover; border-radius:5px;">
+            </div>
+            <h1 style="color:${settings.fontcolor}; font-size:28px; margin:10px 0;">${settings.radioname}</h1>
+            <div id="luna-meta" style="color:${settings.hightlightcolor}; font-size:18px;">Conectando...</div>
+          </div>
+        `);
+
+        function updateMetadata() {
+            $.ajax({
+                url: settings.streamurl + "/stats?sid=" + settings.shoutcastid + "&json=1",
+                dataType: "json",
+                success: function(data) {
+                    if (data && data.songtitle) {
+                        $("#luna-meta").text(data.songtitle);
+                    }
+                }
             });
+        }
 
-            // Google Fonts
-            if (settings.googlefont) {
-                $("head").append("<link href='https://fonts.googleapis.com/css?family="+settings.googlefont+"' rel='stylesheet'>");
-            }
-
-            // Estructura principal (sin botones)
-            var html = "";
-            html += "<div class='lunaradio-info'>";
-            html += "<div class='lunaradio-radioname' style='font-size:2em; margin-bottom:10px;'>" + settings.radioname + "</div>";
-            html += "<div class='lunaradio-songtitle' style='font-size:1.2em; margin-bottom:15px;'>Cargando...</div>";
-            if(settings.coverimage && settings.coverimage !== ""){
-                html += "<div class='lunaradio-cover'><img id='lunaradio-coverimg' src='"+settings.coverimage+"' style='width:200px;height:200px;object-fit:cover;border-radius:"+(settings.coverstyle=="square"?"0":"50%")+";'></div>";
-            }
-            html += "</div>";
-
-            container.append(html);
-
-            // Función para obtener metadatos
-            function getMetadata(){
-                $.ajax({
-                    url: settings.streamurl + "/stats?sid=" + settings.shoutcastid + "&json=1",
-                    dataType: "json",
-                    success: function(data){
-                        if(data && data.songtitle){
-                            container.find(".lunaradio-songtitle").text(data.songtitle);
-                        }
-                        if(data && data.songtitle){
-                            fetchCover(data.songtitle);
-                        }
-                    }
-                });
-            }
-
-            // Buscar portada en iTunes
-            function fetchCover(title){
-                $.ajax({
-                    url: "https://itunes.apple.com/search",
-                    dataType: "jsonp",
-                    data: {
-                        term: title,
-                        media: "music",
-                        entity: "musicTrack",
-                        limit: 1
-                    },
-                    success: function(response){
-                        if(response.results && response.results.length > 0){
-                            var artwork = response.results[0].artworkUrl100.replace("100x100","300x300");
-                            $("#lunaradio-coverimg").attr("src", artwork);
-                        }
-                    }
-                });
-            }
-
-            // Cargar metadatos periódicamente
-            getMetadata();
-            setInterval(getMetadata, settings.metadatainterval);
-        });
+        setInterval(updateMetadata, settings.metadatainterval);
+        updateMetadata();
     };
 })(jQuery);
